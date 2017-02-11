70bb38ed653007f4b46b435edf617fcbaf96081a
Ed Sweeney <ed@table.co>
2017-02-11 10:49:55 -0800

    init

--- devops/pipeline/assembly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd player-master && /usr/bin/sbt assembly && cd ..
-mkdir -p assembly-artifacts
-cp player-master/Dockerfile assembly-artifacts/
-cp -R player-master/devops assembly-artifacts/
-cp -R player-master/target assembly-artifacts/
-
--- devops/pipeline/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "no tests"
-
